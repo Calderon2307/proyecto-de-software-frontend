a68623079f1eb4adf69930ef41cfdaac296617dc
julio22ar <00163922@uca.edu.sv>
2025-11-20 09:50:17 -0600

    ADD: crear equipo en el header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -40,29 +40,33 @@ const Header: React.FC<Props> = ({
         </>
       ) : (
         <>
-          {/* Logo a la izquierda (intocable) */}
+
           <Link to={'/'} className={style.titleLink} title="Go home">
             <h1 className={style.title}>Pokedex</h1>
           </Link>
 
-          {/* Navegación a la derecha */}
+
           <nav className={style.nav}>
-            <Link to="/search/pokemon" className={style.navLink}>
+            <Link to="/search-pokemon" className={style.navLink}>
               Buscar Pokemon
             </Link>
-            <Link to="/search/type" className={style.navLink}>
+            <Link to="/search-type" className={style.navLink}>
               Buscar Tipo
             </Link>
-            <Link to="/search/region" className={style.navLink}>
+            <Link to="/search-region" className={style.navLink}>
               Buscar Region
             </Link>
-            <Link to="/search/item" className={style.navLink}>
+            <Link to="/search-item" className={style.navLink}>
               Buscar Item
             </Link>
-            <Link to="/pokemons" className={style.navLink}>
+            <Link to="/pokedex/pokemon/all" className={style.navLink}>
               Todos los Pokemon
             </Link>
+            <Link to="/create-team" className={style.navLink}>
+              Crear Equipo
+            </Link>
           </nav>
+
         </>
       )}
     </header>
